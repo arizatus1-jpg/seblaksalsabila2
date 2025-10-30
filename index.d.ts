3469991336f208931753698d6bbc22b86de269e4
arizatus1-jpg <arizatus1@gmail.com>
2025-10-30 12:11:24 +0700

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,58 +1,19 @@
-import type { Node } from 'postcss-selector-parser';
-
 /**
- * Options for the calculation of the specificity of a selector
+ * Match `import.meta.resolve` except that `parent` is required (you can pass
+ * `import.meta.url`).
+ *
+ * @param {string} specifier
+ *   The module specifier to resolve relative to parent
+ *   (`/example.js`, `./example.js`, `../example.js`, `some-package`, `fs`,
+ *   etc).
+ * @param {string} parent
+ *   The absolute parent module URL to resolve from.
+ *   You must pass `import.meta.url` or something else.
+ * @returns {string}
+ *   Returns a string to a full `file:`, `data:`, or `node:` URL
+ *   to the found thing.
  */
-export declare type CalculationOptions = {
-    /**
-     * The callback to calculate a custom specificity for a node
-     */
-    customSpecificity?: CustomSpecificityCallback;
-};
-
-/**
- * Compare two specificities
- * @param s1 The first specificity
- * @param s2 The second specificity
- * @returns A value smaller than `0` if `s1` is less specific than `s2`, `0` if `s1` is equally specific as `s2`, a value larger than `0` if `s1` is more specific than `s2`
- */
-export declare function compare(s1: Specificity, s2: Specificity): number;
-
-/**
- * Calculate a custom specificity for a node
- */
-export declare type CustomSpecificityCallback = (node: Node) => Specificity | void | false | null | undefined;
-
-/**
- * Calculate the specificity for a selector
- */
-export declare function selectorSpecificity(node: Node, options?: CalculationOptions): Specificity;
-
-/**
- * The specificity of a selector
- */
-export declare type Specificity = {
-    /**
-     * The number of ID selectors in the selector
-     */
-    a: number;
-    /**
-     * The number of class selectors, attribute selectors, and pseudo-classes in the selector
-     */
-    b: number;
-    /**
-     * The number of type selectors and pseudo-elements in the selector
-     */
-    c: number;
-};
-
-/**
- * Calculate the most specific selector in a list
- */
-export declare function specificityOfMostSpecificListItem(nodes: Array<Node>, options?: CalculationOptions): {
-    a: number;
-    b: number;
-    c: number;
-};
-
-export { }
+export function resolve(specifier: string, parent: string): string;
+export { moduleResolve } from "./lib/resolve.js";
+export type ErrnoException = import("./lib/errors.js").ErrnoException;
+//# sourceMappingURL=index.d.ts.map
