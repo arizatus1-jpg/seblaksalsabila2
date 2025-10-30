d99c6da91151bdeca1cddfd1bb1acf4211f4e8e1
arizatus1-jpg <arizatus1@gmail.com>
2025-10-30 13:35:53 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,95 @@
-"use strict";
-exports.__esModule = true;
-exports.LinesAndColumns = void 0;
-var LF = '\n';
-var CR = '\r';
-var LinesAndColumns = /** @class */ (function () {
-    function LinesAndColumns(string) {
-        this.string = string;
-        var offsets = [0];
-        for (var offset = 0; offset < string.length;) {
-            switch (string[offset]) {
-                case LF:
-                    offset += LF.length;
-                    offsets.push(offset);
-                    break;
-                case CR:
-                    offset += CR.length;
-                    if (string[offset] === LF) {
-                        offset += LF.length;
-                    }
-                    offsets.push(offset);
-                    break;
-                default:
-                    offset++;
-                    break;
-            }
-        }
-        this.offsets = offsets;
-    }
-    LinesAndColumns.prototype.locationForIndex = function (index) {
-        if (index < 0 || index > this.string.length) {
-            return null;
-        }
-        var line = 0;
-        var offsets = this.offsets;
-        while (offsets[line + 1] <= index) {
-            line++;
-        }
-        var column = index - offsets[line];
-        return { line: line, column: column };
-    };
-    LinesAndColumns.prototype.indexForLocation = function (location) {
-        var line = location.line, column = location.column;
-        if (line < 0 || line >= this.offsets.length) {
-            return null;
-        }
-        if (column < 0 || column > this.lengthOfLine(line)) {
-            return null;
-        }
-        return this.offsets[line] + column;
-    };
-    LinesAndColumns.prototype.lengthOfLine = function (line) {
-        var offset = this.offsets[line];
-        var nextOffset = line === this.offsets.length - 1
-            ? this.string.length
-            : this.offsets[line + 1];
-        return nextOffset - offset;
-    };
-    return LinesAndColumns;
-}());
-exports.LinesAndColumns = LinesAndColumns;
-exports["default"] = LinesAndColumns;
+import process from 'node:process';
+import { y as yargsParser, t as trimNewlines, r as redent, n as normalizePackageData, c as camelcaseKeys } from './dependencies.js';
+import { buildOptions } from './options.js';
+import { buildParserOptions } from './parser.js';
+import { checkUnknownFlags, validate, checkMissingRequiredFlags } from './validate.js';
+
+const buildResult = (options, parserOptions) => {
+	const {pkg: package_} = options;
+	const argv = yargsParser(options.argv, parserOptions);
+	let help = '';
+
+	if (options.help) {
+		help = trimNewlines((options.help || '').replace(/\t+\n*$/, ''));
+
+		if (help.includes('\n')) {
+			help = redent(help, options.helpIndent);
+		}
+
+		help = `\n${help}`;
+	}
+
+	normalizePackageData(package_);
+
+	let {description} = options;
+	if (!description && description !== false) {
+		({description} = package_);
+	}
+
+	description &&= help ? redent(`\n${description}\n`, options.helpIndent) : `\n${description}`;
+	help = `${description || ''}${help}\n`;
+
+	const showHelp = code => {
+		console.log(help);
+		process.exit(typeof code === 'number' ? code : 2);
+	};
+
+	const showVersion = () => {
+		console.log(typeof options.version === 'string' ? options.version : package_.version);
+		process.exit(0);
+	};
+
+	if (argv._.length === 0 && options.argv.length === 1) {
+		if (argv.version === true && options.autoVersion) {
+			showVersion();
+		} else if (argv.help === true && options.autoHelp) {
+			showHelp(0);
+		}
+	}
+
+	const input = argv._;
+	delete argv._;
+
+	if (!options.allowUnknownFlags) {
+		checkUnknownFlags(input);
+	}
+
+	const flags = camelcaseKeys(argv, {exclude: ['--', /^\w$/]});
+	const unnormalizedFlags = {...flags};
+
+	validate(flags, options);
+
+	for (const flagValue of Object.values(options.flags)) {
+		if (Array.isArray(flagValue.aliases)) {
+			for (const alias of flagValue.aliases) {
+				delete flags[alias];
+			}
+		}
+
+		delete flags[flagValue.shortFlag];
+	}
+
+	checkMissingRequiredFlags(options.flags, flags, input);
+
+	return {
+		input,
+		flags,
+		unnormalizedFlags,
+		pkg: package_,
+		help,
+		showHelp,
+		showVersion,
+	};
+};
+
+const meow = (helpText, options = {}) => {
+	const parsedOptions = buildOptions(helpText, options);
+	const parserOptions = buildParserOptions(parsedOptions);
+	const result = buildResult(parsedOptions, parserOptions);
+
+	process.title = result.pkg.bin ? Object.keys(result.pkg.bin).at(0) : result.pkg.name;
+
+	return result;
+};
+
+export { meow as default };
--- parser.js
+++ parser.js
@@ -1,45 +1,84 @@
-import * as scope from '../scope/index.js';
-import atrule from '../atrule/index.js';
-import pseudo from '../pseudo/index.js';
-import * as node from '../node/index-parse.js';
+import { d as decamelizeKeys, b as constructParserOptions } from './dependencies.js';
 
-export default {
-    parseContext: {
-        default: 'StyleSheet',
-        stylesheet: 'StyleSheet',
-        atrule: 'Atrule',
-        atrulePrelude(options) {
-            return this.AtrulePrelude(options.atrule ? String(options.atrule) : null);
-        },
-        mediaQueryList: 'MediaQueryList',
-        mediaQuery: 'MediaQuery',
-        condition(options) {
-            return this.Condition(options.kind);
-        },
-        rule: 'Rule',
-        selectorList: 'SelectorList',
-        selector: 'Selector',
-        block() {
-            return this.Block(true);
-        },
-        declarationList: 'DeclarationList',
-        declaration: 'Declaration',
-        value: 'Value'
-    },
-    features: {
-        supports: {
-            selector() {
-                return this.Selector();
-            }
-        },
-        container: {
-            style() {
-                return this.Declaration();
-            }
-        }
-    },
-    scope,
-    atrule,
-    pseudo,
-    node
+const buildParserFlags = ({flags, booleanDefault}) => {
+	const parserFlags = {};
+
+	for (const [flagKey, flagValue] of Object.entries(flags)) {
+		const flag = {...flagValue};
+
+		// `minimist-options` expects `flag.alias`
+		if (flag.shortFlag) {
+			flag.alias = flag.shortFlag;
+			delete flag.shortFlag;
+		}
+
+		if (
+			booleanDefault !== undefined
+				&& flag.type === 'boolean'
+				&& !Object.hasOwn(flag, 'default')
+		) {
+			flag.default = flag.isMultiple ? [booleanDefault] : booleanDefault;
+		}
+
+		if (flag.isMultiple) {
+			flag.type = flag.type ? `${flag.type}-array` : 'array';
+			flag.default = flag.default ?? [];
+			delete flag.isMultiple;
+		}
+
+		if (Array.isArray(flag.aliases)) {
+			if (flag.alias) {
+				flag.aliases.push(flag.alias);
+			}
+
+			flag.alias = flag.aliases;
+			delete flag.aliases;
+		}
+
+		parserFlags[flagKey] = flag;
+	}
+
+	return parserFlags;
 };
+
+const buildParserOptions = options => {
+	let parserOptions = buildParserFlags(options);
+	parserOptions.arguments = options.input;
+
+	parserOptions = decamelizeKeys(parserOptions, {separator: '-', exclude: ['stopEarly', '--']});
+
+	if (options.inferType) {
+		delete parserOptions.arguments;
+	}
+
+	// Add --help and --version to known flags if autoHelp or autoVersion are set
+	if (!options.allowUnknownFlags) {
+		if (options.autoHelp && !parserOptions.help) {
+			parserOptions.help = {type: 'boolean'};
+		}
+
+		if (options.autoVersion && !parserOptions.version) {
+			parserOptions.version = {type: 'boolean'};
+		}
+	}
+
+	parserOptions = constructParserOptions(parserOptions);
+
+	parserOptions.configuration = {
+		...parserOptions.configuration,
+		'greedy-arrays': false,
+	};
+
+	if (parserOptions['--']) {
+		parserOptions.configuration['populate--'] = true;
+	}
+
+	if (!options.allowUnknownFlags) {
+		// Collect unknown options in `argv._` to be checked later.
+		parserOptions.configuration['unknown-options-as-args'] = true;
+	}
+
+	return parserOptions;
+};
+
+export { buildParserOptions };
